49c62c53979b8c2accdaf575abceb2ff1afcfb9e
Noelia Ferruz Capapey <bt1304465@uni-bayreuth.de>
2020-10-17 22:01:08 +0200

    init for conda

--- __init__.py
+++ __init__.py
@@ -1,2 +1,15 @@
 __all__ = ['database','builder','network','scorer','structural']
 
+from protlego.database.data import *
+
+from protlego.network.networks import *
+from protlego.network.colors import *
+#
+from protlego.builder.chimera import *
+from protlego.builder.AAcodes import *
+from protlego.builder.alignments import *
+from protlego.builder.builder import *
+#
+from protlego.scorer.mm_energies import *
+#
+from protlego.structural.analysestrc import *
