5b6558cccf7e5a9c945fbfca84dea98201eef99a
Noelia Ferruz Capapey <bt1304465@uni-bayreuth.de>
2020-10-21 15:32:40 +0200

    conda packages

--- definitions.py
+++ definitions.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-TM_BIN = f"{ROOT_DIR}/TMalign"
+TM_BIN = f"{ROOT_DIR}/builder/TMalign"
 
 logger = logging.getLogger('protlego')
 
